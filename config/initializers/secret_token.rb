45e0853f6923adfb86960dd7154d47290012eee6
Anitha Loganathan <anitha.loganathan@west.cmu.edu>
2011-09-20 20:09:47 -0700

    intial comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onlinebookreview::Application.config.secret_token = '001bad7ca4852efb2eb08f44131abbcb8e4f99d0799238f3c229139fecac344d2def0b8659522a8f6f01a263500c3bd7b1dbaced54e4e95621b8dee2159017dd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Onlinebookreview::Application.config.session_store :cookie_store, :key => '_onlinebookreview_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Onlinebookreview::Application.config.session_store :active_record_store
